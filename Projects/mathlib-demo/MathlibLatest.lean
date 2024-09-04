c1ac3cc9edc01542094fd7a6af62995f1c85448b
root <root@lean-mlir.northamerica-northeast1-b.c.even-card-249812.internal>
2024-09-04 04:36:52 +0000

    chore: nuke existing projects

--- Projects/mathlib-demo/MathlibLatest.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-import ProofWidgets
-
-import MathlibLatest.Bijection
-import MathlibLatest.Logic
-import MathlibLatest.Rational
-import MathlibLatest.Ring
--- Projects/stable/lakefile.lean
+++
@@ -1,9 +0,0 @@
-import Lake
-open Lake DSL
-
-package «stable» where
-  -- add package configuration options here
-
-@[default_target]
-lean_lib «Stable» where
-  -- add library configuration options here
